b68ca1c18d37481afb538c13922640334de86dce
sergeyanton <antonets.sergey@gmail.com>
2025-07-27 16:26:24 +1200

    fix

---
+++ Assets/RestartControlller.cs
@@ -0,0 +1,23 @@
+using UnityEngine; 
+using UnityEngine.SceneManagement;
+
+public class RestartControlller : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void ResetTheGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
+// This script is attached to a button in the UI to reset the game
